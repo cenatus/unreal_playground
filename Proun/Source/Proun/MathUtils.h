610c55bf8bb2b58acdeeea3311ec59d574b9df1c
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2021-11-19 18:29:41 +0100

    new rhizome level and c++ bootstrap

---
+++ Proun/Source/Proun/MathUtils.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MathUtils.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROUN_API UMathUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
---
+++ Proun/Source/Proun/Proun.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Proun.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Proun, "Proun" );
